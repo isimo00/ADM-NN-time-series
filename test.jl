fc7c0aaa70bd00317c524c7653c5a1f4795fb7e9
Irene Simó Muñoz <irenemunozsimo@gmail.com>
2023-04-27 17:56:34 +0200

    RNN started

--- test.jl
+++ test.jl
@@ -1,1 +1,7 @@
+using Flux, Plots, Statistics, MLDatasets
+
+train_x, train_y = MNIST(split=:train)[:]
+test_x, test_y = MNIST(split = :test)[:]
+
+colorview(Gray, train_x[:, :, 1]')
 println("hello world")
